0eefc93219ce1ef950f3976eb4475b2f4d558782
ookamitai <ookamitai2007@gmail.com>
2024-12-07 17:24:52 +0800

    [chore]: init repo

---
+++ Typeman/TypemanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypemanApp.swift
+//  Typeman
+//
+//  Created by ookamitai on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TypemanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
